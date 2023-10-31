892d12e1a71afd8d2965245cdbc2238aa325a117
Ahmed Hosny <ahmhosny2002@gmail.com>
2023-11-01 00:01:48 +0200

    Hottest fix

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+
+"""Module that is about locked class."""
+
+
 class LockedClass:
+    """
+    Only permit attribute creation for those named first_name.
+    """
     __slots__ = ["first_name"]

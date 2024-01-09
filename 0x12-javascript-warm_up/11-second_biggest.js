0449083feeb867fdd1727749685f92b575baec80
Ahmed Hosny <ahmhosny2002@gmail.com>
2024-01-09 14:26:31 +0200

    semistandard --fix is used

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,12 +5,13 @@ if (process.argv.length <= 3) {
   const arr = process.argv.slice(2).map(Number);
   let num1 = Math.max(arr[0], arr[1]);
   let num2 = Math.min(arr[0], arr[1]);
-  for (let i = 2; i < arr.length; i++)
+  for (let i = 2; i < arr.length; i++) {
     if (arr[i] > num1) {
       num2 = num1;
-      num1 = arr[i]
+      num1 = arr[i];
     } else if (arr[i] > num2) {
-      num2 = arr[i]
+      num2 = arr[i];
     }
+  }
   console.log(num2);
 }
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-let arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < arr.length; i++)
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < arr.length; i++) {
   console.log(arr[i]);
+}
